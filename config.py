ec4610266f5ded3a0a2232a03d42b7e086f4bb52
Marco <marco.conciatori@unipr.it>
2024-04-10 18:20:54 +0200

    changed parameters

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 verbose = True
-video_name = 'vehicle-counting.mp4'
-# video_name = '01270009.AVI'
+# video_name = 'vehicle-counting.mp4'
+video_name = '01270009.avi'
 model_name = 'yolov8x.pt'
-selected_classes = [2, 3, 5, 7]
+# selected_classes = [2, 3, 5, 7]
+selected_classes = [0, 1, 2, 3]
 
