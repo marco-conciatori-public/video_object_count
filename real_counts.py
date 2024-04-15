c7e4994b8525f95b52ee98027b30c5b86afb9f30
Marco <marco.conciatori@unipr.it>
2024-04-15 18:06:34 +0200

    added real count info for testing

---
+++ real_counts.py
@@ -0,0 +1,55 @@
+by_file_name = {
+    # '01250003.mp4':,
+    '01270009.mp4': {
+        'in_counts': 0,
+        'out_counts': 1,
+        'class_wise_count': {
+            'person': 1,
+        },
+    },
+    '01280016.mp4': {
+        'in_counts': 2,
+        'out_counts': 0,
+        'class_wise_count': {
+            'person': 2,
+        },
+    },
+    '02030028.mp4': {
+        'in_counts': 2,
+        'out_counts': 0,
+        'class_wise_count': {
+            'person': 2,
+        },
+    },
+    '02040042.mp4': {
+        'in_counts': 5,
+        'out_counts': 0,
+        'class_wise_count': {
+            'person': 5,
+        },
+    },
+    # '02140058.mp4':,
+    # '02150063.mp4':,
+    '02180074.mp4': {
+        'in_counts': 0,
+        'out_counts': 1,
+        'class_wise_count': {
+            'car': 1,
+        },
+    },
+    # '02180080.mp4':,
+    '02180090.mp4': {
+        'in_counts': 8,
+        'out_counts': 0,
+        'class_wise_count': {
+            'person': 4,
+            'motorcycle': 4,
+        },
+    },
+    # '02190100.mp4',
+    # '02190102.mp4',
+    # '02200103.mp4',
+    # '02250143.mp4',
+    # '03140182.mp4',
+    # '03240226.mp4',
+}
