7de6e1bcaa580d0ed7538566172580e9cd9879bd
Marco <marco.conciatori@unipr.it>
2024-04-17 17:53:41 +0200

    refactoring

--- test_algorithm.py
+++ test_algorithm.py
@@ -4,11 +4,13 @@ import count_objects
 
 
 true_counts_dict = true_counts.by_file_name
+file_counter = 0
 for file_name, counts in true_counts_dict.items():
-    print(f'file: {file_name}')
+    print(f'file number {file_counter} ("{file_name}")')
     predicted_counts = count_objects.count_objects_(video_name=file_name, verbose=False)
     print(f'\tpredicted_counts: {predicted_counts}')
     print(f'\ttrue_counts:      {counts}')
     # utils.show_difference_dicts(true=counts, predicted=predicted_counts)
+    file_counter += 1
     print('--------------------------------------------------')
 
