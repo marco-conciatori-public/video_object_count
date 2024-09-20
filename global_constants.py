a7cfdff76d3c60f1cb0a382df5031036a134a4fc
Marco Conciatori <conciatori.marco91@gmail.com>
2024-09-20 15:13:06 +0200

    changed parameters

--- global_constants.py
+++ global_constants.py
@@ -3,5 +3,5 @@ MODEL_FOLDER = 'models/'
 OUTPUT_FOLDER = 'output/'
 CONFIG_PARAMETER_PATH = 'config.yaml'
 
-# VIDEO_FORMAT = 'mp4'
-VIDEO_FORMAT = 'avi'
+VIDEO_FORMAT = 'mp4'
+# VIDEO_FORMAT = 'avi'
